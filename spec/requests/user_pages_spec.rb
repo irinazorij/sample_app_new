4752493760a7c6df6e599ed1598811d8711cbaee
irinazorij <irina.zorij@gmail.com>
2016-10-01 23:16:35 +0300

    Beginning Cucumba

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,7 +21,7 @@ end
      it "should not create a user" do
        expect { click_button submit }.not_to change(User, :count)
      end
-   end
+  end
 
    describe "with valid information" do
      before do
@@ -31,10 +31,18 @@ end
        fill_in "Confirmation", with: "foobar"
      end
 
-     it "should create a user" do
-       expect { click_button submit }.to change(User, :count).by(1)
+   describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  end
+
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+       end
      end
    end
  end
-end
-    
